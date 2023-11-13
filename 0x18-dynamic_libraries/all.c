0bae4c37bc1dfd2e2b56a0cb5eaba3ff0af6385c
Nimona123 <argano4444@gmail.com>
2023-11-13 05:45:04 -0800

    0x18-dynamic_libraries by Nimona123

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * add.
+ * @a:
+ * @b:
+ * Return:
+ */
+int add (int a, int b)
+{
+	return (a+b);
+}
+/**
+ * add.
+ * @a:
+ * @b:
+ * Return:
+ */
+int mul (int a, int b)
+{
+        return (a*b);
+}
